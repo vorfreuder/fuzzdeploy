61fe8a647cbdb784e956513352332b570e45bff5
vorfreuder <1484630652@qq.com>
2025-06-16 22:05:58 +0800

    🐞 fix: fetch script of memlock

--- fuzzers/memlock/fetch.sh
+++ fuzzers/memlock/fetch.sh
@@ -5,6 +5,5 @@ if [ -d "$FUZZER/repo" ]; then
     exit 0
 fi
 
-git clone --depth 1 https://github.com/wcventure/MemLock-Fuzz
-mv MemLock-Fuzz/tool/MemLock "$FUZZER/repo"
-rm -rf MemLock-Fuzz
+git clone --depth 1 https://github.com/wcventure/MemLock-Fuzz "$FUZZER/repo"
+mv "$FUZZER/repo/tool/MemLock"/* "$FUZZER/repo"
